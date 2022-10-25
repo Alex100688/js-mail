a6caabcf342be24c67f71ee6de66f0448b4ee732
Alex100688 <alessiozingaro2@gmail.com>
2022-10-25 21:40:55 +0200

    Aggiunge le condizoni

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ const mail= ["marco@gmail.com", "luca@gmail.com", "sandro@libero.it"];
 let askEmail= prompt("Inserisci la tua email");
 for (let i = 0; i < mail.length; i++) {
     const element = mail[i];
-    
+    let askEmail=mail;
+    if(askEmail==mail){
+        alert("Sei registrato");
+        break;
+        
+    }
+    else if(askEmail!=mail.length){
+        alert("Non sei registrato");
+    }
 }
