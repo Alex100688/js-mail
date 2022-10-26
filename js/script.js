8cf35ba1382c5372dbf671abee9f5c5daef4a75a
Alex100688 <alessiozingaro2@gmail.com>
2022-10-26 02:04:25 +0200

    Aggiunge le due condizioni

--- js/script.js
+++ js/script.js
@@ -3,13 +3,11 @@ const mail= ["marco@gmail.com", "luca@gmail.com", "sandro@libero.it"];
 let askEmail= prompt("Inserisci la tua email");
 for (let i = 0; i < mail.length; i++) {
     const element = mail[i];
-    let askEmail=mail;
-    if(askEmail==mail){
+    if(register==askEmail){
         alert("Sei registrato");
-        break;
-        
+       
     }
-    else if(askEmail!=mail.length){
+    else if(register!=askEmail){
         alert("Non sei registrato");
     }
 }
